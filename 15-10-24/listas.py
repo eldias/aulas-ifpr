d0f86a8a440d46cb25c3bbc07af68a18b01b2eaf
Elias Dias Junior <eldias@gmail.com>
2024-10-15 19:34:28 -0300

    Create listas.py

---
+++ 15-10-24/listas.py
@@ -0,0 +1,31 @@
+import os
+
+# Tipo de Listas
+
+lista1 = []
+lista2 = list()
+lista3 = [1,2,3,4]
+lista4 = list(lista3)
+lista5 = [1,2,'Texto', 'abacate', 'uva']
+
+# Exibição
+
+os.system('cls')
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+print(lista5)
+print("Tamanho da lista 3: ", len(lista3))
+print("Tamanho da lista 5: ", len(lista5))
+print("\n\n")
+
+tecla = input("Any keys to continue: ")
+
+if tecla == any:
+    os.system('cls')
+    pass
+
+print(lista5[0])
+print(lista5[3])
