d8412aae2064466639948cf7512765a1648cc0aa
Elias Dias Junior <eldias@gmail.com>
2024-10-16 20:48:00 -0300

    Update exer_18_listas.py

--- 15-10-24/exer_18_listas.py
+++ 15-10-24/exer_18_listas.py
@@ -1,31 +1,26 @@
 """
     2. Implementar um programa que, a partir do usuário, receba um conjunto de números e troque o primeiro e o último número informados de posição. O usuário deverá informar inicialmente qual é a quantidade de números da lista.
 """
+
+# Pedir ao usuário a quantidade de números a computar
 quantidade_numeros = int(input("Informe a quantidade de números: "))
+# Cria lista vazia
 lista = list()
+# i recebe 1 
 i = 1
-
-
+# Loop em i para informar os números a inserir na lista usando append
 while i <= quantidade_numeros:
     num = int(input("Informe um número: "))
     lista.append(num) 
     i += 1
-    
-prim_lista = lista[0]
-ult_lista = lista[-1]
-
-print(prim_lista, ult_lista)
-print(lista)
-
-print(f"\nTrocando...\n")
-
-remove_prim = lista.pop(prim_lista)
-print("Retirando index 0")
-print(lista, "\n")
-
-lista.index(-1, remove_prim)
-remove_ult = lista.pop(ult_lista)
-lista.index(-1, remove_ult)
-
-print(prim_lista, ult_lista)
+# define variáveis para o receber o primeiro e últimos números da lista
+primeiro = lista[0]
+ultimo = lista[-1]
+# retirar primeiro e último item da lista
+lista.pop(0)
+lista.pop(-1)
+# Inserir as variáveis do primeiro e últimos números da lista de volta, invertendo a ordem deles
+lista.insert(0, ultimo)
+l = lista[-1]
+lista.insert(l, primeiro)
 print(lista)
