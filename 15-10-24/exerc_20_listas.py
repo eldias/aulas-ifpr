58a4c7cd58fa95f71d1b9cccde6faf4f83b655e2
Elias Dias Junior <eldias@gmail.com>
2024-10-22 19:30:28 -0300

    aula 22-10-24

--- 15-10-24/exerc_20_listas.py
+++ 15-10-24/exerc_20_listas.py
@@ -1,14 +1,27 @@
 """
     4. Implementar um programa que, a partir do usuário, receba um conjunto de nomes de pessoas e os apresente em ordem alfabética. O usuário deverá informar inicialmente qual é a quantidade de nomes.
 """
-
+n = int(input("Informe a quantidade de nomes: "))
 lista = list() # criação de lista
-parar = 'fim' # define comando na variável stop
+# parar = 'fim' # define comando na variável stop
 
-while True: # abre loop while
+for i in range(n):
+    nome = input("Informe um nome: ")
+    lista.append(nome)
+
+    
+lista.sort()
+print(f"\nSua lista ordenada:")
+[print(item) for item in lista]
+
+
+
+
+
+""" while True: # abre loop while
     termo = input("Insira os nomes da lista, pare digitando 'fim'.\n=> ") # define val com entrada de texto
     if termo == parar: # loop se val for stop
         break # pare o loop
     lista.append(termo) # adicione os temos em lista
 
-print(f"Sua lista ordenada é: {sorted(lista)}") # imprima a lista ordenada
+print(f"Sua lista ordenada é: {sorted(lista)}") # imprima a lista ordenada """
