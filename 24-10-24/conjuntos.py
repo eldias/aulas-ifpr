84ba5a970a10748d2b40a2264548f114e0c7289e
Elias Dias Junior <eldias@gmail.com>
2024-10-24 20:47:39 -0300

    subir

---
+++ 24-10-24/conjuntos.py
@@ -0,0 +1,46 @@
+""" 
+import os
+
+os.system('cls') """
+
+frases = {
+    "Você se encontra em uma floresta escura.",
+    "Uma voz misteriosa sussurra no seu ouvido.",
+    "Um cavaleiro surge do nevoeiro.",
+    "Você encontra um baú cheio de ouro.",
+    "Um dragão bloqueia o seu caminho.",
+    "Uma taverna acolhedora aparece à distância.",
+    "Você sente uma presença mágica ao seu redor.",
+    "Um feitiço é lançado contra você.",
+    "Você encontra uma espada lendária.",
+    "Uma bruxa oferece ajuda, mas a que custo?",
+    "Você entra em uma cidade movimentada.",
+    "Um estranho misterioso oferece uma missão.",
+    "Você encontra um pergaminho antigo.",
+    "Uma ponte levadiça impede o seu avanço.",
+    "Você ouve gritos vindos de uma caverna.",
+    "Uma tempestade se aproxima rapidamente.",
+    "Você descobre um mapa do tesouro.",
+    "Um grupo de ladrões tenta roubar você.",
+    "Você encontra uma poção curativa.",
+    "Um ancião sábio oferece conselhos.",
+    "Você sente uma energia sombria vindo do norte.",
+    "Um portal mágico aparece diante de você.",
+    "Você avista um castelo imponente à distância.",
+    "Uma batalha épica está prestes a começar.",
+    "Você encontra um amuleto mágico.",
+    "Um feitiço de proteção é ativado.",
+    "Você encontra uma vila pacífica.",
+    "Um monstro aparece de repente.",
+    "Você encontra um aliado inesperado.",
+    "Uma lenda antiga é revelada a você."
+}
+
+# print(frases)
+
+entrada_aleatoria = frases.pop()
+print(entrada_aleatoria)
+
+
+
+
---
+++ 24-10-24/times.py
@@ -0,0 +1,29 @@
+"""
+1. Implementar um programa que verifique quais estudantes praticam simultaneamente atletismo e basquete. O usuário deve informar os estudantes de atletismo e, depois, os de basquete. O programa deve exibir os alunos que participam de ambas as atividades.
+"""
+
+atletismo = set()
+basquete = set()
+parar = 'fim' # define comando na variável stop
+
+while True: # abre loop while
+    termo = input("Insira um nome para adicionar ao time de atletismo: ") # define val com entrada de texto
+    if termo == parar: # loop se val for stop
+        break # pare o loop
+    atletismo.add(termo) # adicione os temos em lista
+
+print('Sua lista é de jogadores de atletismo são: ',atletismo) # imprima a lista
+
+while True: # abre loop while
+    termo = input("Insira um nome para adicionar ao time de basquete: ") # define val com entrada de texto
+    if termo == parar: # loop se val for stop
+        break # pare o loop
+    basquete.add(termo) # adicione os temos em lista
+
+print('Sua lista é de jogadores de basquete são: ',basquete) # imprima a lista
+
+jogadores = atletismo & basquete
+
+print(f"\nOs jogadores que estão em ambos times são:\n{jogadores}")
+
+
