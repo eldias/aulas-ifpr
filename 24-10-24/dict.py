23f9f7a4fa1ce6b54781038b4af1043c762ee6d2
Elias Dias Junior <eldias@gmail.com>
2024-10-24 21:49:37 -0300

    Create dict.py

---
+++ 24-10-24/dict.py
@@ -0,0 +1,11 @@
+referencia = dict()
+
+referencia = {
+    "Autor":"Mary Shelley",
+    "Nome do livro":"Frankestein",
+    "colors": ["red", "white", "blue"]
+}
+
+for valor, chave in referencia.items():
+    print(valor, chave, sep=" : ")
+    
