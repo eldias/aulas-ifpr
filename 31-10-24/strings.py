76458a98f9e908ee6a4063e402a99e8c04983cff
Elias Dias Junior <eldias@gmail.com>
2024-10-31 20:16:21 -0300

    Create strings.py

---
+++ 31-10-24/strings.py
@@ -0,0 +1,14 @@
+string = "Elias Dias Junior"
+
+""" print(len(string), "chars")
+
+print("start in: ", string[:7])
+print("ends in: " ,string[7:])
+print("reverse: ", string[::-1]) 
+
+print(string.count('a'))"""
+
+print(string.find('ias'))
+
+
+
