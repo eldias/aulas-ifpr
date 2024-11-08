f9acdad32aa9d7e0201ad0dcc2c9e9211a69bcfe
eldias <eldias@gmail.com>
2024-11-08 12:46:11 -0300

    Update exerc_22_1.py

--- 05-11-24/exerc_22_1.py
+++ 05-11-24/exerc_22_1.py
@@ -2,13 +2,17 @@
     1.  Implementar um programa que solicite ao usuário seu nome completo e salve essa informação em um arquivo de texto. O usuário deve informar o nome completo e o caminho do arquivo onde o dado será armazenado.
 """
 
-
+# definição de variáveis
 nome: str = input("Qual seu nome? ")
 caminho: str = input("Onde gostaria de salvar o arquivo? ")
+
+
+# loop para confirmar caminho e  nome de arquivo
 if caminho == "":
     caminho = "05-11-24/"
 arquivo: str = "nomes.txt"
 abspath: str = caminho + arquivo
 
+# gravando arquivo com nome declarado
 with open(abspath, "w") as acesso:
     acesso.write(nome)
